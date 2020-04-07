074d89bb16a152f75794aecf8246dbfe03baa2b0
Matheus Lichtnow <matheusrobert8@gmail.com>
2020-04-06 20:26:07 -0700

    feature: added shape for selected item

--- lib/paint/normal_curve_painter.dart
+++ lib/paint/normal_curve_painter.dart
@@ -9,14 +9,37 @@ class NormalCurvePainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint();
-    // set the color property of the paint
     paint.color = this.color;
-
-    // center of the canvas is (x,y) => (width/2, height/2)
     var center = Offset(this.circleSize / 2, this.circleSize / 2);
-
-    // draw the circle on centre of canvas having radius 75.0
     canvas.drawCircle(center, this.circleSize / 2, paint);
+
+    canvas.drawOval(Rect.fromCenter(center: center, width: 50.0, height: 60.0), paint);
+
+    var path = Path();
+    path.moveTo(0, this.circleSize / 2);
+
+
+    path.cubicTo(0, this.circleSize, -this.circleSize / 1.5, this.circleSize,  0, this.circleSize);
+    // path.quadraticBezierTo(0 - 20.0, this.circleSize, 0, this.circleSize);
+    path.lineTo(this.circleSize, this.circleSize);
+    path.close();
+    canvas.drawPath(path, paint);
+
+
+    path.moveTo(this.circleSize, this.circleSize / 2);
+    
+
+    path.cubicTo(this.circleSize, this.circleSize, 1.75*this.circleSize, this.circleSize,  this.circleSize, this.circleSize);
+    path.lineTo(this.circleSize / 2, this.circleSize);
+    path.lineTo(this.circleSize, this.circleSize / 2);
+    // path.lineTo(this.circleSize, this.circleSize / 2);
+    // path.quadraticBezierTo(size.width*0.25, size.height*0.77, size.width*0.52, size.height*0.84);
+    // path.quadraticBezierTo(size.width*0.74, size.height*0.92, size.width, size.height*0.84);
+    // path.lineTo(size.width, size.height);
+    // path.lineTo(0, size.height);
+    path.close();
+    canvas.drawPath(path, paint);
+    
   }
 
   @override
